135250b9823ed76093eb9b50278fdfca6deb238d
Brendan Taylor <whateley@gmail.com>
2009-07-16 22:01:37 -0600

    functioning mplayer.rb

---
+++ mplayer.rb
@@ -0,0 +1,153 @@
+require 'thread'
+
+require 'monitor'
+
+class Status
+  include MonitorMixin
+
+  def initialize mp, io
+    super()
+
+    @io = io
+
+    @prop_cond = self.new_cond
+
+    @thread = Thread.new do
+      puts "starting"
+      @io.each_line { |line| self.got_line(line) }
+      mp.next # i suspect this will break horribly occasionally
+    end
+  end
+
+  def got_line line
+    self.synchronize do
+      puts "<<< " + line
+      if line.match /^ANS_PERCENT_POSITION=(.*)/
+        @percent_pos = $1.to_i
+        @prop_cond.signal
+      end
+    end
+  end
+
+  attr_reader :prop_cond, :percent_pos
+end
+
+class MPlayer
+  attr_reader :playlist, :playing
+
+  def initialize
+    @playlist = []
+    @io = nil
+    @status = nil
+  end
+
+  def run cmd
+    puts '>>> ' + cmd
+    @io.puts cmd
+
+    # running a command unpauses us (unless it was a pause command of course)
+    @paused = false unless cmd == 'pause'
+  end
+
+  def pl_append file
+    @playlist << file
+
+    if !@playing
+      play_file @playlist.first
+    end
+  end
+
+  def pl_append_dir path
+    if File.directory?(path + '/VIDEO_TS')
+      pl.append(path)
+    end
+
+    Dir[path + '/*'].sort.each do |fn|
+      if File.file? n
+        pl.append(fn)
+      end
+    end
+  end
+
+  def play_file path
+    cmd = 'mplayer -fs -noconsolecontrols -slave -quiet'
+
+    path_esc = " '" + path.gsub(/'/, "\\\'") + "'"
+
+    if File.directory?(path) and File.directory?(path + '/VIDEO_TS')
+      cmd += ' -dvd-device'
+      cmd += path_esc
+      cmd += ' dvd://'
+    else
+      cmd += path_esc
+    end
+
+    puts cmd.inspect
+
+    stop if @io
+    @io = IO.popen cmd, "r+"
+    @status = Status.new(self, @io)
+
+    @paused = false
+    @playing = path
+  end
+
+  def percent_pos
+    # if it's paused, avoid running a command (it will unpause it)
+    return @status.percent_pos if @paused
+
+    @status.synchronize do
+      run 'get_percent_pos'
+      @status.prop_cond.wait
+      @status.percent_pos
+    end
+  end
+
+  def stop
+    unless @io.closed?
+      run 'stop'
+      @io.close
+    end
+  rescue Errno::EPIPE
+    # whatever, we're good
+  ensure
+    @io = nil
+    @playing = nil
+  end
+
+  def nav diff
+    curr_idx = @playlist.index(@playing)
+    return unless curr_idx
+
+    next_file = @playlist[curr_idx + diff]
+    return unless next_file
+
+    play_file next_file
+  end
+
+  def next; nav +1; end
+  def prev; nav -1; end
+
+  def toggle_pause
+    run 'pause'
+    @paused = !@paused
+  end
+
+  def seek pos
+    if ["+"[0], "-"[0]].member? pos[0]
+      pos = pos[1..-1]
+      run "seek #{pos} 0"
+    elsif pos[-1].chr == '%'
+      pos = pos[0..-2]
+      run "seek #{pos} 1"
+    else
+      run "seek #{pos} 2"
+    end
+  end
+end
+
+$mp = MPlayer.new
+
+Dir['/home/data/music/Skinny Puppy/Last Rights/*'].sort.each do |file|
+  $mp.pl_append(file)
+end

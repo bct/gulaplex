ad3e759115764d12088259dbac873adf9e96bffc
Brendan Taylor <whateley@gmail.com>
2011-01-01 21:10:26 -0700

    complete the rename

--- config.rb
+++ config.rb
@@ -4,4 +4,4 @@ EXCLUDE    = ['/bulk/downloads']
 COMMIT_ID   = `git log -n1 --oneline | cut -f1 -d\\ `
 MEDIA_EXTENSIONS  = /\.(mp3|ape|flac|ogg|mkv|avi|wmv|iso)$/i
 IGNORE_EXTENSIONS = /\.(nfo|tbn)$/i
-REPO_PATH        = 'sqlite:/home/xuser/booble/booble.db'
+REPO_PATH        = 'sqlite:/home/xuser/gulaplex/gulaplex.db'
